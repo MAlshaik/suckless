2edcb3ea3939200fc15ccd7e32af694c1fae595d
N0H3dy <workmail2776@gmail.com>
2022-09-11 00:52:34 -0400

    dwm files

---
+++ slock/config.def.h
@@ -0,0 +1,21 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#458588",   /* during input */
+	[FAILED] = "#fb4934",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Locked";
+
+/* text color */
+static const char * text_color = "#ebdbb2";
+
+/* text size (must be a valid size) */
+static const char * font_name = "9x15";
