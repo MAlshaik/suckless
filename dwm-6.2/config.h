6864b59e39d96b37954cce659c264de6f4e638b8
N0H3dy <workmail2776@gmail.com>
2022-09-12 16:09:35 -0400

    color edits

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -14,11 +14,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#458588";
+static const char col_gray3[]       = "#928374";
+static const char col_gray4[]       = "#1d2021";
+static const char col_cyan[]        = "#458588";
+static const char col_font[]        = "#ebdbb2";
 static const char col_red[]        = "#FF0000";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
